9d815c65cf4ccc434f48248ced647faaac72141b
btoledo <braulio.toledo@neoris.com>
2023-03-21 07:51:08 -0600

    routing complete

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -9,14 +9,51 @@ sap.ui.define([
 		onDisplayNotFound : function () {
 			this.getRouter().getTargets().display("notFound", { fromTarget : "home" });
 		},
+		// Configurations
 		onNavToWorkcenters : function (){
 			this.getRouter().navTo("workcenterPanel");
 		},
 		onNavToInspection : function (){
 			this.getRouter().navTo("inspectionPanel");
+		},
+		onNavToMaterial : function (){
+			this.getRouter().navTo("materialPanel");
+		},
+		onNavToCharacteristic : function (){
+			this.getRouter().navTo("characteristicPanel");
+		},
+		onNavToRule : function (){
+			this.getRouter().navTo("rulePanel");
+		},
+		onNavToActionCatalog : function (){
+			this.getRouter().navTo("actionCatalogPanel");
+		},
+
+		// Quality
+		onNavToUsage : function (){
+			this.getRouter().navTo("usagePanel");
+		},
+		onNavToStatistic : function (){
+			this.getRouter().navTo("statisticPanel");
+		},
+		onNavToAlarm : function (){
+			this.getRouter().navTo("alarmPanel");
+		},
+		onNavToDashboard : function (){
+			this.getRouter().navTo("dashboardPanel");
+		},
+		onNavToLotBatch : function (){
+			this.getRouter().navTo("lotBatchPanel");
+		},
+		onNavToResultRecording : function (){
+			this.getRouter().navTo("resultRecordingPanel");
+		},
+		onNavToBulkRecording : function (){
+			this.getRouter().navTo("bulkRecordingPanel");
+		},
+		onNavToSampleDashboard : function (){
+			this.getRouter().navTo("sampleDashboardPanel");
 		}
-
-
 
 		// onNavToConfigurations : function (){
 		// 	this.getRouter().navTo("configurationsGrid");
