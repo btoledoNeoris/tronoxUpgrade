3142d3a6ba91f805b88d60d850e5b5d5e9589434
btoledo <braulio.toledo@neoris.com>
2023-12-13 22:02:14 -0600

    Se actualiza controlador y vista para enviar datos de filtros a controlador

--- webapp/controller/quality/resultRecording/ResultRecordingPanel.controller.js
+++ webapp/controller/quality/resultRecording/ResultRecordingPanel.controller.js
@@ -1,53 +1,68 @@
 sap.ui.define([
 	'tronox/controller/LayerController',
-	"sap/ui/model/json/JSONModel",
+    'sap/m/MessageBox',
     'sap/m/MessageToast',
-	"../../../tools/build",
+	"../../../tools/handler",
 ],
-function (LayerController,JSONModel,MessageToast,serverMII) {
+function (LayerController,MessageBox,MessageToast,handler) {
 	"use strict";
 	return LayerController.extend("tronox.controller.configurations.workcenters.WorkcenterPanel", {
+        PLANT: "1100",
+        WORKCENTER: "LAB_____",
 		onInit: function () {
-            this.getPlant();
             this.getWcenter();
 		},
+        getI18nText: function(sKey) {
+            var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            return oResourceBundle.getText(sKey);
+        },
         onChange: function(oEvent){
-            console.log(oEvent.getParameters())
+            console.log("oC")
             // MessageToast.show("change event fired! \n Selected Item id: " + oEvent.getParameters().id);
         },
-        getPlant: function() {
-            let plant = '1100';
-            let url = serverMII.getEnv() == "LOCAL" ? '../../localServices/configurations/PlantByUserSelectQuery.json' : serverMII.get() + getTrx.getPlantByUserSelectQuery;
-            var data = [];
-
-            var oModel = new JSONModel(url);
-            oModel.loadData(url, {"Param.1": plant}, true, "POST");
-            oModel.attachRequestCompleted(function() {
-                let allRes = oModel.getData().Rowsets.Rowset[0].Row;
-                console.log({allRes})
-                allRes.map(itm => { data.push(itm) })
-                console.log({data})
-                oModel.setData(data);
-                console.log(oModel)
-                // this.getView().setModel(oModel);
-
-            }.bind(this))
-
-        },
         getWcenter: function() {
-            let url = serverMII.getEnv() == "LOCAL" ? '../../localServices/configurations/WorkCenterDetailSelectQuery.json' : serverMII.get() + getTrx.getWorkCenterDetailSelectQuery;
-			console.log({url})
-            var data = [];
+            console.log("WC")
+        },
+        onSearch: async function(){
+            const oView = this.getView();
+            const oModel = this.getView().getModel();
+            const Plant = this.PLANT;
+            const WorkCenter = this.WORKCENTER;
+            const sMaterialComboBox = this.byId("materialComboBox").getValue();
+            const sMaterialInput = oView.byId("input-material").getValue();
+            const Material = sMaterialInput ? sMaterialInput : sMaterialComboBox;
+            const sBatchComboBox = oView.byId("batchComboBox").getValue();
+            const sBatchInput = oView.byId("input-batch").getValue();
+            const BatchCode = sBatchInput ? sBatchInput : sBatchComboBox;
+            const StartDate = oView.byId("dateInit").getValue();
+            const EndDate = oView.byId("dateEnd").getValue();
 
-            var oModel = new JSONModel(url);
-            oModel.loadData(url, {}, true, "POST");
-            oModel.attachRequestCompleted(function() {
-                let allRes = oModel.getData().Rowsets.Rowset[0].Row;
-                allRes.map(itm => { data.push(itm) })
-                oModel.setData(data);
-                this.getView().setModel(oModel);
+            const parameters = { Plant, WorkCenter, Material, BatchCode, StartDate, EndDate };
+            console.log({ parameters })
+            console.log(oServices.getResultsRecording)
+            try {
+                this.getView().setBusy(true);
+                let getData = '../../localServices/quality/ResultsRecording.json';
+                // const data = await handler.requestData(oTransactions.getEventsDetailsByArea, parameters);
+               
+                const data = await handler.requestData(getData, parameters);;
+                if (data[0]?.Row?.length === 0) {
+                    MessageToast.show(this.getI18nText("messageError"), { duration: 2000 });
+                    console.error(this.getI18nText("messageError"));
+                } else {
+                    console.log(data)
+                    oModel.setProperty("/table", []);
+                    console.log(oModel.getProperty("/table"));
+                }
+            } catch (error) {
+                oModel.setProperty("/table", []);
+                MessageToast.show(this.getI18nText("messageError"), { duration: 2000 });
+                console.error(error);
+                this.getView().setBusy(false);
+            } finally {
+                this.getView().setBusy(false);
+            }
 
-            }.bind(this))
         }
 	});
 });
